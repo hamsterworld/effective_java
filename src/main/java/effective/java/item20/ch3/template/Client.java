1119b28071b84fa26b6e1544890b1cf9d04453fb
오인석 <ssoboro1@gmail.com>
2022-12-11 16:50:24 +0900

    item 20 보충

--- src/main/java/effective/java/item20/ch3/template/Client.java
+++ src/main/java/effective/java/item20/ch3/template/Client.java
@@ -2,7 +2,18 @@ package effective.java.item20.ch3.template;
 
 public class Client {
     public static void main(String[] args) {
+
         FileProcessor fileProcessor = new Plus("number.txt");
         System.out.println(fileProcessor.process());
+
+        FileProcessor fileProcessor1 = new FileProcessor("number.txt") {
+            @Override
+            protected int getResult(int result, int number) {
+                return result + number;
+            }
+        };
+
+        System.out.println(fileProcessor1.process());
+
     }
 }
