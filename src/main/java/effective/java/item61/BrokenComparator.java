3fd2d643728c1df93de4c207307c480037ca0b1d
오인석 <ssoboro1@gmail.com>
2023-02-14 20:56:05 +0900

    item 61

--- src/main/java/effective/java/item61/BrokenComparator.java
+++ src/main/java/effective/java/item61/BrokenComparator.java
@@ -1,13 +1,25 @@
 package effective.java.item61;
 import java.util.*;
 
+/**
+ * 박싱된타입과 박싱되지않은 타입의 차이점은 크게 3가지이다. (꼭 Integer 이런것에만 국한하지말고 박싱된 class 들에대해서 그럴수있다고 생각하자)
+ *  1. 기본타입은 값만 가지고 있으나, 박싱된 타입은 식별성을 가지고있다.
+ *  그래서 박싱된타입이 같은 값을 가지고있어도 다른인스턴스이므로 다르게 인식될수있다.
+ *  (논리적동치성의 Value Class 와 관련된 내용인듯)
+ *  2.박싱된타입은 null 을 가질수있다.
+ *  3. 기본타입이 박싱된 타입보다 메모리,cpu 측면에서 훨씬더 효율적이다.
+ *
+ *  어쨋든 위 3가지를 생각하지않고 코딩하면 큰 문제를 일으킬수있다는점이다.
+ *
+ */
 // 코드 61-1 잘못 구현된 비교자 - 문제를 찾아보자! (359쪽)
 public class BrokenComparator {
     public static void main(String[] args) {
-
+        // 1번의 특징으로 인해 발생한 문제
 //        Comparator<Integer> naturalOrder =
 //                (i, j) -> (i < j) ? -1 : (i == j ? 0 : 1);
 
+        // 그래서 수정했다.
        // 코드 61-2 문제를 수정한 비교자 (359쪽)
         Comparator<Integer> naturalOrder = (iBoxed, jBoxed) -> {
             int i = iBoxed, j = jBoxed; // 오토박싱
--- src/main/java/effective/java/item61/Unbelievable.java
+++ src/main/java/effective/java/item61/Unbelievable.java
@@ -2,10 +2,21 @@ package effective.java.item61;
 
 // 코드 61-3 기이하게 동작하는 프로그램 - 결과를 맞혀보자! (360쪽)
 public class Unbelievable {
+    /**
+     * 기본타입과 박싱타입을 혼용하는 연산에서는
+     * 기본적으로 모두 박싱이 풀리게된다.
+     * 박싱이 풀리고 박싱 value 가 null 이라면 NPE 를 터트리게된다.
+     * 해결법은 간단하긴하다 i 의 type 을 int 로 변경시켜주면된다.
+     */
     static Integer i;
-
     public static void main(String[] args) {
         if (i == 42)
             System.out.println("믿을 수 없군!");
+
+        Long sum = 0L;
+        for(long i = 0L; i<Long.MIN_VALUE; i++){
+            sum += i;
+        }
+        // 위 코드는 끔찍히 느리다.
     }
 }
